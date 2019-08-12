375971ac5fbe6395a25b313e9d0131abdb20924d
Özalp Döndüren <ozalp@donduren.com>
2019-08-12 19:41:40 +0300

    first commit with ImageDemoWPF app

---
+++ ImageDemoWPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using ImageDemoWPF.Models;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageDemoWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public ImageFileInfo ImageFile { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (!string.IsNullOrEmpty(propertyName) && PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void OpenImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                                    "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                                    "Portable Network Graphic (*.png)|*.png";
+            if (openFileDialog.ShowDialog(this) != true)
+                return;
+
+            ImageFile = new ImageFileInfo(openFileDialog.FileName);
+            RaisePropertyChanged("ImageFile");
+        }
+    }
+}
---
+++ ImageDemoWPF/Models/ImageFileInfo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Security;
+using System.Windows.Media.Imaging;
+
+namespace ImageDemoWPF.Models
+{
+    public class ImageFileInfo
+    {
+        [SecuritySafeCritical]
+        public ImageFileInfo(string fileName)
+        {
+            if (fileName == null)
+                throw new ArgumentNullException(nameof(fileName));
+
+            fileInfo = new FileInfo(fileName);
+        }
+        private FileInfo fileInfo;
+
+
+       
+        public BitmapImage BitmapImage
+        {
+            get
+            {
+                if (bitmapImage == null)
+                {
+                    bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.StreamSource = fileInfo.OpenRead();
+                    bitmapImage.EndInit();
+                }
+                return bitmapImage;
+            }
+        }
+        private BitmapImage bitmapImage;
+
+        /// <summary>Gets a string representing the directory's full path.</summary>
+        /// <returns>A string representing the directory's full path.</returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// <see langword="null" /> was passed in for the directory name. </exception>
+        /// <exception cref="T:System.IO.PathTooLongException">The fully qualified path is 260 or more characters.</exception>
+        /// <exception cref="T:System.Security.SecurityException">The caller does not have the required permission. </exception>
+        public string DirectoryName { get { return fileInfo.DirectoryName; } }
+
+        /// <summary>Gets a value indicating whether a file exists.</summary>
+        /// <returns>
+        /// <see langword="true" /> if the file exists; <see langword="false" /> if the file does not exist or if the file is a directory.</returns>
+        public bool Exists { get { return fileInfo.Exists; } }
+
+        /// <summary>Gets the full path of the directory or file.</summary>
+        /// <returns>A string containing the full path.</returns>
+        /// <exception cref="T:System.IO.PathTooLongException">The fully qualified path and file name is 260 or more characters.</exception>
+        /// <exception cref="T:System.Security.SecurityException">The caller does not have the required permission. </exception>
+        public virtual string FullName { get { return fileInfo.FullName; } }
+
+        /// <summary>
+        /// The image height
+        /// </summary>
+        public int Height { get { return BitmapImage.PixelHeight; } }
+
+        /// <summary>
+        /// The image width.
+        /// </summary>
+        public int Width { get { return BitmapImage.PixelWidth; } }
+
+        /// <summary>Gets the size, in bytes, of the current file.</summary>
+        /// <returns>The size of the current file in bytes.</returns>
+        /// <exception cref="T:System.IO.IOException">
+        /// <see cref="M:System.IO.FileSystemInfo.Refresh" /> cannot update the state of the file or directory. </exception>
+        /// <exception cref="T:System.IO.FileNotFoundException">The file does not exist.-or- The <see langword="Length" /> property is called for a directory. </exception>
+        public long Length { get { return fileInfo.Length; } }
+
+        /// <summary>Gets the name of the image file.</summary>
+        /// <returns>The name of the file.</returns>
+        public string Name { get { return fileInfo.Name; } }
+
+
+    }
+}
