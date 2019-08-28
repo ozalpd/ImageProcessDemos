325bc93df19cd495006717e036f4cdd0a0e2fc47
Özalp Döndüren <ozalp@donduren.com>
2019-08-28 19:24:40 +0300

    Added Magick.NET demo application

---
+++ MagickDemo/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MagickDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow;
+            if (e.Args.Length > 0 && File.Exists(e.Args[0]))
+            {
+                mainWindow = new MainWindow(e.Args[0]);
+            }
+            else
+            {
+                mainWindow = new MainWindow();
+            }
+
+            mainWindow.Show();
+        }
+
+    }
+}
---
+++ MagickDemo/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MagickDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow(string startupFile) : this()
+        {
+            StartupFile = new FileInfo(startupFile);
+        }
+
+        public FileInfo StartupFile { get; private set; }
+
+        private void SaveICO(FileInfo inputFile)
+        {
+            using (MagickImage image = new MagickImage(inputFile))
+            {
+                image.Settings.SetDefine(MagickFormat.Icon, "auto-resize", "256,128,64,48,32,16");
+                image.Write(inputFile.Replace(inputFile.Extension, ".ico"));
+            }
+        }
+    }
+}
